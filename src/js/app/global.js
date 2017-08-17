7fcb5b81f41088cdcb780e6c42dd529f8160ced5
Miloslav Nosek <nosekmiloslav@gmail.com>
2017-08-17 10:11:19 +0200

    Fix wrong path to settings in production

--- src/js/app/global.js
+++ src/js/app/global.js
@@ -3,13 +3,14 @@ import Helper from "./Helper";
 const h = new Helper;
 
 export let userItems;
+export let settings;
 
 if (h.detectEnvironment() === "development") {
     userItems = "./src/data/list.json";
+    settings = "./src/data/settings.json";
 } else if (h.detectEnvironment() === "production") {
     userItems = global.process.resourcesPath + "/app/src/data/list.json";
+    settings = global.process.resourcesPath + "/app/src/data/settings.json";
 } else {
-    throw `global | userItems: ${userItems}`;
+    throw "Can't detect environment";
 }
-
-export const settings = "./src/data/settings.json";
